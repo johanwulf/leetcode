43a7a1a7252675f3632ea1727a0415d3e5e963b8
johanwulf <johanwulf98@gmail.com>
2023-09-10 17:11:21 +0200

    solution: add solution to min stack problem

---
+++ min-stack/solution.py
@@ -0,0 +1,18 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.min_stack.append(min(val, self.min_stack[-1] if self.min_stack else val))
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.min_stack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
